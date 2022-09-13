5e1e8a678a6d10734c50f228af81e5cdc994e034
SatouTappei <stutpi.signup@gmail.com>
2022-09-13 16:22:54 +0900

    プレイヤーのダッシュ＆残像を追加

--- Assets/Sato/PlayerMove.cs
+++ Assets/Sato/PlayerMove.cs
@@ -12,12 +12,22 @@ public class PlayerMove : MonoBehaviour
     PlayerInputEvent _pie;
     /// <summary>プレイヤーの移動速度の倍率</summary>
     [SerializeField] float _speedMag;
+    /// <summary>走る速度の倍率</summary>
+    [SerializeField] float _dashMag;
+    /// <summary>残像</summary>
+    [SerializeField] GameObject[] _afterEffects;
     /// <summary>プレイヤーの画像のアニメーター</summary>
     Animator _spriteAnim;
     /// <summary>入力された移動ベクトル</summary>
     Vector3 _velocity;
     /// <summary>前フレームの移動ベクトル</summary>
     Vector3 _prevVelo;
+    /// <summary>ダッシュしているかどうか</summary>
+    bool _dash = false;
+    /// <summary>残像を出すまでのカウント</summary>
+    float _afterEffectCount;
+    /// <summary>出す残像の番号</summary>
+    int _afterEffectIndex;
 
     void Awake()
     {
@@ -35,8 +45,29 @@ public class PlayerMove : MonoBehaviour
     {
         float hori = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
+        _dash = Input.GetKey(KeyCode.LeftShift);
 
         _velocity = new Vector3(hori, vert, 0);
+
+        if (_dash)
+        {
+            _spriteAnim.speed = _dashMag;
+
+            _afterEffectCount += Time.deltaTime;
+            if(_afterEffectCount > 0.1)
+            {
+                _afterEffectCount = 0;
+                Instantiate(_afterEffects[_afterEffectIndex], transform.position, Quaternion.identity);
+            }
+        }
+        else
+        {
+            _spriteAnim.speed = 1;
+            _afterEffectCount = 0;
+        }
+
+
+
 
         if (_prevVelo != _velocity)
         {
@@ -44,21 +75,25 @@ public class PlayerMove : MonoBehaviour
             {
                 _spriteAnim.Play("Left");
                 _pie.RayDir = -transform.right;
+                _afterEffectIndex = 3;
             }
             else if (hori == 1)
             {
                 _spriteAnim.Play("Right");
                 _pie.RayDir = transform.right;
+                _afterEffectIndex = 2;
             }
             else if (vert == 1)
             {
                 _spriteAnim.Play("Up");
                 _pie.RayDir = transform.up;
+                _afterEffectIndex = 1;
             }
             else if (vert == -1)
             {
                 _spriteAnim.Play("Down");
                 _pie.RayDir = -transform.up;
+                _afterEffectIndex = 0;
             }
         }
 
@@ -67,6 +102,6 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rb.velocity = _velocity * _speedMag;
+        _rb.velocity = _velocity * _speedMag * (_dash ? _dashMag : 1);
     }
 }
